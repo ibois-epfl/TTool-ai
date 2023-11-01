a64cc5d2d0e9248284de2a90aa2ddb17eaee1f74
faymanns <florian.aymanns@t-online.de>
2023-11-01 16:24:04 +0100

    Implement dataloader
    Converts a video into frames and randomly splits them into test and training data.

--- src/datasetservice/worker.py
+++ src/datasetservice/worker.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
+import random
 
+import cv2
 from celery import Celery
 
 celery = Celery(__name__)
@@ -10,5 +12,28 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
 @celery.task(name="process_video")
 def process_video(path: pathlib.Path):
-    print(path)
+    directory = path.parents[0]
+    train_dir = directory / "train"
+    val_dir = directory / "val"
+    train_dir.mkdir()
+    val_dir.mkdir()
+    cap = cv2.VideoCapture(str(path))
+
+    if not cap.isOpened():
+        raise RuntimeError("Could not open capture.")
+
+    frame_idx = 0
+    while cap.isOpened():
+        if random.random() < 0.8:
+            out_dir = train_dir
+        else:
+            out_dir = val_dir
+        ret, frame = cap.read()
+        if ret:
+            out_file = out_dir / f"{frame_idx}.png"
+            cv2.imwrite(str(out_file), frame)
+            frame_idx += 1
+        else:
+            break
+    cap.release()
     return True
