909a586aac3bfb20eff1efda91a8bb0082cf5c7d
faymanns <florian.aymanns@t-online.de>
2023-11-01 16:21:38 +0100

    Fix missing commits to the database
    Previously the SQL commands where never actually executed because the commit calls were missing.

--- src/datasetservice/main.py
+++ src/datasetservice/main.py
@@ -18,13 +18,15 @@ curs.execute(
     "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
 )
 for table in curs.fetchall():
-    tables.append(table)
+    tables.append(table[0])
 if "datasets" not in tables:
     curs.execute(
         """CREATE TABLE datasets (dataset_id varchar(255), label varchar(255),
         video_path varchar(255), video_hash varchar(255), n_images int,
         n_train_images int, n_val_images int, celery_task_id varchar(255))"""
     )
+curs.close()
+conn.commit()
 
 
 app = fastapi.FastAPI()
@@ -32,10 +34,13 @@ app = fastapi.FastAPI()
 
 @app.get("/datasets")
 def get_list_of_datasets():
+    curs = conn.cursor()
     curs.execute(
         """SELECT dataset_id, label, video_path,
             n_images, n_train_images, n_val_images FROM datasets"""
     )
+    curs.close()
+    conn.commit()
     results = curs.fetchall()
     return fastapi.responses.JSONResponse(results)
 
@@ -68,21 +73,26 @@ async def upload_video(video: fastapi.UploadFile, label: str, status_code=201):
 
     task = worker.process_video.delay(str(video_path))
 
+    curs = conn.cursor()
     curs.execute(
         f"""INSERT INTO datasets (dataset_id, label,
         video_path, video_hash, celery_task_id) VALUES ('{dataset_id}',
         '{label}' , '{str(video_path)}', '{video_hash}', '{task.id}')"""
     )
+    curs.close()
+    conn.commit()
     return fastapi.responses.JSONResponse({"dataset_id": dataset_id})
 
 
 @app.get("/trainings/{dataset_id}")
 def get_status(dataset_id):
+    curs = conn.cursor()
     curs.execute(
         f"""SELECT celery_task_id FROM datasets
         WHERE datasets.dataset_id = '{dataset_id}'"""
     )
     task_id = curs.fetchone()
+    curs.close()
     task_result = celery.result.AsyncResult(task_id)
     result = {
         "dataset_id": task_id,
