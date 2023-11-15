b46533ab34c9187448c45d71ac17c6389b392271
sushidelivery <nazgul160796@icloud.com>
2023-11-15 18:16:56 +0100

    ADD-WIP: custom serialization function for sqlalchemy model

--- src/controlservice/models/postgres_model.py
+++ src/controlservice/models/postgres_model.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Enum
 from config.postgres_config import Base
 
 
-class DataLoaderStatus(str, Enum):
+class DataLoaderStatus(Enum):
     PENDING = 'pending'
     PROCESSING = 'processing'
     COMPLETED = 'completed'
@@ -14,8 +14,15 @@ class VideoDB(Base):
     id = Column(Integer, primary_key=True, index=True)
     label = Column(String)
     video_path = Column(String)
-    video_hash = Column(String)
-    upload_status = DataLoaderStatus.PENDING
+    video_hash = Column(String, unique=True)
+    upload_status = Column(String, default=DataLoaderStatus.PENDING)
 
 
-
+def serialize_video_db(video):
+    return {
+        "id": video.id,
+        "label": video.label,
+        "video_path": video.video_path,
+        "video_hash": video.video_hash,
+        "upload_status": video.upload_status
+    }
