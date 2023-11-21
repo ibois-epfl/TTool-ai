3dab28e488341344d479931876c3036b7929211b
aymanns <florian.aymanns@epfl.ch>
2023-11-21 09:20:39 +0100

    Implement basic worker structure after removing celery such that RabbitMQ can be used directly

--- src/trainingservice/worker.py
+++ src/trainingservice/worker.py
@@ -1,23 +1,60 @@
-import os
+class Callback:
+    def __init__(self):
+        pass
 
-from celery import Celery
+    def connect(self, url):
+        pass
 
-import training
+    def callback(self, ch, method, properties, body):
+        pass
 
-rabbitmq_user = os.environ.get("RABBITMQ_DEFAULT_USER")
-rabbitmq_password = os.environ.get("RABBITMQ_DEFAULT_PASS")
-rabbitmq_host = os.environ.get("RABBITMQ_HOST")
-rabbitmq_port = os.environ.get("RABBITMQ_PORT")
-train_queue = os.environ.get("RABBITMQ_TRAIN_QUEUE")
+class TrainingWorker:
+    def __init__(self, queue):
+        pass
 
-celery = Celery(__name__)
-celery.conf.broker_url = (
-    f"amqp://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_host}:{rabbitmq_port}"
-)
-celery.conf.task_routes = {"train": {"queue": train_queue}}
+    def connect(self, user, password, host, port):
+        pass
 
+    def start_consuming(self, callback):
+        pass
 
-@celery.task(name="train")
-def train(**kwargs):
-    training.train(**kwargs)
-    return True
+    def close_connection(self):
+        pass
+
+if __name__ == "__main__":
+    USER = os.environ.get("RABBITMQ_DEFAULT_USER")
+    PASSWORD = os.environ.get("RABBITMQ_DEFAULT_PASS")
+    HOST = os.environ.get("RABBITMQ_HOST")
+    PORT = os.environ.get("RABBITMQ_PORT")
+    QUEUE = os.environ.get("RABBITMQ_DATA_QUEUE")
+
+    worker = TrainingWorker(QUEUE)
+
+    # The rabbitmq container for testing sometimes needs
+    # more time to start so we try to connect multiple times.
+    while True:
+        try:
+            worker.connect(user=USER, password=PASSWORD, host=HOST, port=PORT)
+            print("Connection to RabbitMQ succeeded.")
+            break
+        except pika.exceptions.AMQPConnectionError:
+            print("Connection to RabbitMQ failed.")
+            time.sleep(1)
+            continue
+
+    # Connect to postgres db
+    USER = os.environ.get("POSTGRES_USER")
+    PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    HOST = os.environ.get("POSTGRES_HOST")
+    PORT = os.environ.get("POSTGRES_PORT")
+    DB = os.environ.get("POSTGRES_DB")
+    DB_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+
+    callback = Callback()
+    callback.connect(DB_URL)
+
+    try:
+        worker.start_consuming(callback=callback.callback)
+    finally:
+        worker.close_connection()
+        callback.close_connection()
