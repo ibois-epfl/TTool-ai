47597a24451e91c33e37c40b25728698e609a270
aymanns <florian.aymanns@epfl.ch>
2023-11-22 16:35:01 +0100

    First implementation of the Callback class

--- src/trainingservice/training_worker.py
+++ src/trainingservice/training_worker.py
@@ -1,14 +1,51 @@
+import json
 import os
+import pathlib
 import time
 
 import pika
 import sqlalchemy
+import sqlalchemy.orm
 
 import training
 
 
 class Base(sqlalchemy.orm.DeclarativeBase):
     pass
+
+
+class Status(str, sqlalchemy.Enum):
+    PENDING = "pending"
+    TRAINING = "training"
+    DONE = "done"
+    FAILED = "failed"
+
+
+class TrainingDB(Base):
+    __tablename__ = "trainings"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+
+    data_dirs = sqlalchemy.Column(sqlalchemy.String)
+    max_epochs = sqlalchemy.Column(sqlalchemy.Integer)
+    batch_size = sqlalchemy.Column(sqlalchemy.Integer)
+
+    training_hash = sqlalchemy.Column(sqlalchemy.String, unique=True)
+
+    status = sqlalchemy.Column(sqlalchemy.String, default=Status.PENDING)
+    logdir = sqlalchemy.Column(sqlalchemy.String)
+    weights = sqlalchemy.Column(sqlalchemy.String)
+    trace_file = sqlalchemy.Column(sqlalchemy.String)
+
+
+class TrainingParams:
+    def __init__(self, bstr):
+        param_dict = json.loads(bstr.decode("utf-8"))
+        self.max_epochs = param_dict["max_epochs"]
+        self.batch_size = param_dict["batch_size"]
+        self.data_dirs = tuple(param_dict["data_dirs"])
+
+    def __hash__(self):
+        return hash((self.max_epochs, self.batch_size, *self.data_dirs))
 
 
 class Callback:
@@ -19,12 +56,58 @@ class Callback:
         self.engine = sqlalchemy.create_engine(database_url)
         Base.metadata.create_all(self.engine)
 
+    def _add_info(
+        self, training_hash, log_dir=None, trace_file=None, weights=None, status=None
+    ):
+        stmt = sqlalchemy.select(TrainingDB).where(
+            TrainingDB.training_hash == training_hash
+        )
+        with sqlalchemy.orm.Session(self.engine) as session:
+            training_entry = session.scalars(stmt).one()
+            if log_dir is not None:
+                training_entry.log_dir = log_dir
+            if trace_file is not None:
+                training_entry.trace_file = trace_file
+            if weights is not None:
+                training_entry.weights = weights
+            if status is not None:
+                training_entry.status = status
+            session.commit()
+
     def callback(self, ch, method, properties, body):
-        try:
-            training_params = dict(body.decode("utf-8"))
-            training.train(**training_params)
-        except Exception as e:
-            print(e)
+        training_params = TrainingParams(body)
+        training_hash = hash(training_params)
+        new_training = TrainingDB(
+            max_epochs=training_params.max_epochs,
+            data_dirs=training_params.data_dirs,
+            training_hash=training_hash,
+        )
+        with sqlalchemy.orm.Session(self.engine) as session:
+            session.add(new_training)
+            session.commit()
+
+        log_dir = pathlib.Path(f"/data/{training_hash}")
+        log_dir.mkdir()
+        self._add_info(training_hash, log_dir=log_dir)
+        # try:
+        self._add_info(training_hash, status=Status.TRAINING)
+        weights_file, trace_file = training.train(
+            max_epochs=training_params.max_epochs,
+            batch_size=training_params.batch_size,
+            data_dirs=training_params.data_dirs,
+            log_dir=log_dir,
+        )
+        self._add_info(
+            training_hash,
+            status=Status.DONE,
+            weights=weights_file,
+            trace_file=trace_file,
+        )
+        # except Exception as e:
+        #     self._add_info(training_hash, status=Status.FAILED)
+
+    def close_connection(self):
+        self.engine.dispose()
 
 
 class TrainingWorker:
@@ -72,6 +155,7 @@ if __name__ == "__main__":
 
     callback = Callback()
     callback.connect(DB_URL)
+    time.sleep(3600)
 
     try:
         worker.start_consuming(callback=callback.callback)
