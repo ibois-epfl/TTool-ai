a99a2f3100d44cff029842eb65c5b503fbac9c70
aymanns <florian.aymanns@epfl.ch>
2023-11-23 13:18:04 +0100

    Test for TrainingWorker

--- src/trainingservice/tests/test_worker.py
+++ src/trainingservice/tests/test_worker.py
@@ -1,10 +1,12 @@
 import json
 import pathlib
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
+import pika
+import pytest
 import sqlalchemy
 import training
-from training_worker import Base, Callback, TrainingDB, TrainingParams
+from training_worker import Base, Callback, TrainingDB, TrainingParams, TrainingWorker
 
 
 @patch("training_worker.Base.metadata.create_all", spec=Base.metadata.create_all)
@@ -65,3 +67,49 @@ def test_Callback(
     mock_create_engine.assert_called_once_with("URL")
     mock_session.assert_called_with(mock_create_engine.return_value)
     mock_select.assert_called_with(mock_training_db)
+
+
+@pytest.fixture
+def mock_blocking_connection():
+    with patch(
+        "pika.BlockingConnection", spec=pika.BlockingConnection
+    ) as mock_connection:
+        yield mock_connection
+
+
+@pytest.fixture
+def mock_channel(mock_blocking_connection):
+    mock_channel = mock_blocking_connection.return_value.channel.return_value
+    return mock_channel
+
+
+def test_training_worker(mock_blocking_connection, mock_channel):
+    mock_callback = Mock(spec=Callback.callback)
+    credentials = pika.PlainCredentials(username="test_user", password="test_password")
+    connection_params = pika.ConnectionParameters(
+        host="test_host", port=5672, credentials=credentials
+    )
+
+    # Run
+    worker = TrainingWorker(queue="test_queue")
+    worker.connect(
+        user="test_user",
+        password="test_password",
+        host="test_host",
+        port=1234,
+    )
+    worker.start_consuming(callback=mock_callback)
+    # Emulate the behaviour of the real connection
+    worker.connection.is_closed = False
+
+    worker.close_connection()
+
+    # Asserts
+    mock_blocking_connection.assert_called_once_with(connection_params)
+    mock_channel.queue_declare.assert_called_once_with(queue="test_queue", durable=True)
+    mock_channel.basic_qos.assert_called_once_with(prefetch_count=1)
+    mock_channel.basic_consume.assert_called_once_with(
+        queue="test_queue", on_message_callback=mock_callback
+    )
+    mock_channel.start_consuming.assert_called_once()
+    mock_blocking_connection.return_value.close.assert_called_once()
