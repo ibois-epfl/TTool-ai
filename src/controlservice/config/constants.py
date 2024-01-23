7a6669ef68ffaa576ab091ccb8de99e08104460c
sushidelivery <nazgul160796@icloud.com>
2024-01-23 16:14:53 +0100

    REMOVE: reorganized the files

--- src/controlservice/config/constants.py
+++
@@ -1,2 +0,0 @@
-
-DOCKER_VIDEO_DIR = "/media/train_videos/"
--- src/controlservice/models/train_config_api.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-
-class TrainConfig(BaseModel):
-    classes: List[str]
-    epochs: int
-    batch_size: int
